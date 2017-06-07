268d2e8795781a8eb9cb72c3423265e5a1c2ea96
Eduardo Ugolini <edu.ugolini2@gmail.com>
2017-06-06 22:08:48 -0300

    Adicionadas entidades

---
+++ src/AppBundle/Entity/Endereco.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Endereco
+ *
+ * @ORM\Table(name="endereco", indexes={@ORM\Index(name="usuario_id", columns={"usuario_id"})})
+ * @ORM\Entity
+ */
+class Endereco
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="rua", type="string", length=80, nullable=false)
+     */
+    private $rua;
+
+    /**
+     * @var \Usuario
+     *
+     * @ORM\ManyToOne(targetEntity="Usuario")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="usuario_id", referencedColumnName="id")
+     * })
+     */
+    private $usuario;
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set rua
+     *
+     * @param string $rua
+     * @return Endereco
+     */
+    public function setRua($rua)
+    {
+        $this->rua = $rua;
+
+        return $this;
+    }
+
+    /**
+     * Get rua
+     *
+     * @return string 
+     */
+    public function getRua()
+    {
+        return $this->rua;
+    }
+
+    /**
+     * Set usuario
+     *
+     * @param \AppBundle\Entity\Usuario $usuario
+     * @return Endereco
+     */
+    public function setUsuario(\AppBundle\Entity\Usuario $usuario = null)
+    {
+        $this->usuario = $usuario;
+
+        return $this;
+    }
+
+    /**
+     * Get usuario
+     *
+     * @return \AppBundle\Entity\Usuario 
+     */
+    public function getUsuario()
+    {
+        return $this->usuario;
+    }
+}
